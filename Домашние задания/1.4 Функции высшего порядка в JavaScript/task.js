83c87e28606a4a117dbda9aa6d94f3e90955a284
losiik <49565548+losiik@users.noreply.github.com>
2025-03-09 00:20:56 +0300

    hw1

--- Домашние задания/1.4 Функции высшего порядка в JavaScript/task.js
+++ Домашние задания/1.4 Функции высшего порядка в JavaScript/task.js
@@ -1,9 +1,49 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
+    let cache = [];
   
-}
+    function wrapper(...args) {
+      const hash = md5(args);
+      let objectInCache = cache.find((item) => item.hash === hash);
+  
+      if (objectInCache) {
+        console.log("Из кеша: " + objectInCache.value);
+        return "Из кеша: " + objectInCache.value;
+      }
+  
+      let result = func(...args);
+      cache.push({ hash, value: result });
+      if (cache.length > 5) {
+        cache.shift();
+      }
+  
+      console.log("Вычисляем: " + result);
+      return "Вычисляем: " + result;
+    }
+  
+    return wrapper;
+  }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
+    let timeoutId;
+    
+    function wrapper(...args) {
+      if (!timeoutId) {
+        func(...args);
+        wrapper.count++;
+      }
+      wrapper.allCount++;
+      
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        func(...args);
+        wrapper.count++;
+      }, delay);
+    }
   
-}
+    wrapper.count = 0;
+    wrapper.allCount = 0;
+  
+    return wrapper;
+  }
